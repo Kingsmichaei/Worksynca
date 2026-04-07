fc0dbf19904c86583b1fb7ec8a336706c0815bde
Kings Michael <kingsmichael.official@gmail.com>
2026-04-07 21:39:22 +0100

    first commit

--- attendance/create_superuser.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.models import User
-
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser(
-        username='MICHAEL',
-        email='kingsmichael.x@gmail.com',
-        password='MICHAEL123!'
-    )
-    print('Superuser created!')
